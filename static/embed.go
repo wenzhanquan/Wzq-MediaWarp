151efb034a7ee3910351b2c5c9d61e913807204b
wenzhanquan <974527510@qq.com>
2026-05-17 00:10:07 +0800

    Update embed.go

--- static/embed.go
+++ static/embed.go
@@ -5,11 +5,4 @@ import (
 )
 
 //go:embed embyExternalUrl/embyWebAddExternalUrl/embyLaunchPotplayer.js
-//go:embed emby-web-mod/actorPlus/actorPlus.js
-//go:embed emby-web-mod/emby-swiper/emby-swiper.js
-//go:embed emby-web-mod/emby-tab/emby-tab.js
-//go:embed emby-web-mod/fanart_show/fanart_show.js
-//go:embed emby-web-mod/itemSortForNewDevice/itemSortForNewDevice.js
-//go:embed emby-web-mod/playbackRate/playbackRate.js
-//go:embed emby-web-mod/trailer/trailer.js
 var EmbeddedStaticAssets embed.FS
